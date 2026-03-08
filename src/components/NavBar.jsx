ad574e5c7f9cc8c3ef50269ffa3703d552757b2f
nickonyi <nickoluoch1130@gmail.com>
2026-03-08 13:21:44 +0300

    feat: add Homepage, NavBar, and HeroBar components; include lucide-react dependency; create global styles

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { LogIn } from "lucide-react";
+function NavBar() {
+  return (
+    <nav className="sticky left-0 top-0 w-full shadow-md flex justify-between px-12 py-6">
+      <h1 className="text-xl font-semibold">InnerCircles</h1>
+      <div className="hidden md:flex gap-4 text-neutral-500">
+        <button>Home</button>
+        <button>Circes</button>
+        <button>Post</button>
+      </div>
+      <div className="login">
+        <button className="flex gap-2">
+          <span>
+            <LogIn />
+          </span>{" "}
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+import HeroBar from "../components/HeroBar";
+function Homepage() {
+  return (
+    <>
+      <NavBar />
+      <HeroBar />
+    </>
+  );
+}
+
+export default Homepage;

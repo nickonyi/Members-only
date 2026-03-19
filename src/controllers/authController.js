f619f28feadbc7837b074241b15594c6e7a5c9e8
nickonyi <nickoluoch1130@gmail.com>
2026-03-19 18:38:08 +0300

    feat:form field validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,23 @@
 import { matchedData, validationResult } from "express-validator";
 
 export const getSignup = (req, res) => {
-  res.render("signup", { title: "Sign up", errors: [] });
+  res.render("signup", { title: "Sign up", errors: {}, formData: {} });
 };
 
 export const postSignup = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log("hayo");
+    const mapperErrors = {};
+    errors.array().forEach((err) => {
+      if (!mapperErrors[err.path]) {
+        mapperErrors[err.path] = err.msg;
+      }
+    });
 
     return res.status(400).render("signup", {
       title: "sign up",
-      errors: errors.array(),
+      errors: mapperErrors,
       formData: req.body,
     });
   }

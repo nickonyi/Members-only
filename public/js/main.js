e5843e260bb1289094706a4f3dc318ce406897a2
nickonyi <nickoluoch1130@gmail.com>
2026-04-20 16:53:17 +0300

    feat: implement join circle modal with secret code input and update button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 const userBtn = document.getElementById("user-menu-btn");
 const dropdown = document.getElementById("user-dropdown");
+const modal = document.getElementById("join-modal");
+const openBtn = document.getElementById("open-join-modal");
+const closeBtn = document.getElementById("close-modal");
 
 document.addEventListener("click", (e) => {
   if (userBtn?.contains(e.target)) {
@@ -7,4 +10,14 @@ document.addEventListener("click", (e) => {
   } else if (!dropdown?.contains(e.target)) {
     dropdown?.classList.add("hidden");
   }
+});
+
+openBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
 });
